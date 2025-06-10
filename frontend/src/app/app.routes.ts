5f45b1aab322cc42edec2c7ce507e93d2c4e8ddf
Shan Ahmad Saqib <shan.ahmadsaqib@student.ap.be>
2025-06-10 21:16:41 +0200

    router updaten

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ClientComponent } from './client/client.component';
+import { ClientFormComponent } from './client/client-form/client-form.component';
+import { FoodPickupComponent } from './food-pickup/food-pickup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: ClientComponent },
+    { path: 'clientform', component: ClientFormComponent },
+    { path: 'food-pickup', component: FoodPickupComponent }
+];
